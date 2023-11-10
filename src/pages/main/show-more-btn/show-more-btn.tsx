32113a0d037cfbc436b2f13947c1707e890c2a41
Elena Manshina <Lenika2000@yandex.ru>
2023-11-10 19:00:50 +0300

    Контроль и ограничения (часть 2)

---
+++ src/pages/main/show-more-btn/show-more-btn.tsx
@@ -0,0 +1,10 @@
+type ShowMoreBtnProps = {
+  handleClick: () => void;
+};
+export function ShowMoreBtn({ handleClick }: ShowMoreBtnProps): JSX.Element {
+  return (
+    <div className='catalog__more'>
+      <button className='catalog__button' type='button' onClick={handleClick}>Show more</button>
+    </div>
+  )
+}
